91f0279d62ea109b3a213074ac2fcba4de3d2411
iscc7 <30551381+iscc7@users.noreply.github.com>
2017-11-15 09:34:51 +0800

    Update sa.m

--- sa.m
+++ sa.m
@@ -3,32 +3,38 @@ clc,clear
 f = @(x)-(x-2)^2;
 
 T = 1e4;
-T_decrease = 0.9999;
+T_decrease = 0.9995;
 x_min = -10;
 x_max = 10;
 
-range = 2;
+range = 0.2;
 
 x0 = x_min+rand*(x_max-x_min);
 y0 = f(x0);
 box = zeros(1,100000);
+box_x = zeros(1,100000);
 for i = 1:100000
-    x1 = x0 + (rand-rand)*(range);
+    x1 = x0 + rand*range-(range)/2;
     if x1<x_min;
         x1 = x_min;
     end
     if x1 > x_max
-        x1 = x_max;
+        x1 = x_max;   
     end
     y1 = f(x1);
     error = y1-y0;
     if error >= 0
         x0 = x1;
+        y1 = y0;
     elseif exp(error/T)>rand;
         x0 = x1;
+        y1 = y0;
     end
     box(i) = exp(error/T);
+    box_x(i) = x0;
     T = T*T_decrease;
 end
 x0
 plot(box)
+figure
+plot(box_x)
